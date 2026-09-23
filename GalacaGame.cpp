bb1d5d511555b05939e639b3ab80d4e982354b0a
matthewbrice5 <matthewbrice5@gmail.com>
2026-09-23 13:36:12 -0500

    Add files via upload

--- GalacaGame.cpp
+++ GalacaGame.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 #include <vector>
 #include "raylib.h"
+using namespace std;
+
+
 
 
 struct Bullet{  //Keeps track of the bullets x and y coordiante 
     int x;
     int y;
 };
+
+std::vector<Bullet> firingbullets(150);
 
 struct HardEnemy{
     int x;
@@ -22,9 +27,25 @@ struct BasicEnemy{ // Holds the top left and bottom right of the image, with a m
     int y;
     int bottomx;
     int bottomy;
+    bool isHit = false;
     Texture2D enemyimage = LoadTexture("Graphics/enemy1.png");
+    Texture2D explosion = LoadTexture("Graphics/explosion.png");
+    int opaqueness = 255;
+    bool dead = false;
     
     void updateframe(){
+        
+        checkCollision(firingbullets);
+        
+        if(isHit && !dead){
+            opaqueness-= 5;
+                if(opaqueness < 5){
+                    dead = true;
+                }
+            return;
+        }
+        
+        
         if(moveBacknForth){ //If moveBacknForth is true, this tells the enemy how to do that 
             if(right){ //Moves the enemy to the right
                 x+=2;
@@ -32,38 +53,57 @@ struct BasicEnemy{ // Holds the top left and bottom right of the image, with a m
                 y += 2;
                 bottomy +=2;
                 moveValue++;
-            if(moveValue == 40){
-                right = false;}
-           } 
-           else { //Moves the enemy to the left
+                    if(moveValue == 40){
+                        right = false;
+                        }
+            } else { //Moves the enemy to the left
                 x-=2;
                 bottomx -=2;
                 y += 2;
                 bottomy +=2; 
                 moveValue--;
-                if(moveValue == 0){
-                    right = true;
-                }
-                
+                    if(moveValue == 0){
+                        right = true;
+                    }  
            }
 
-        } else{ //Basic Enemy goes straight down
+        } 
+        else{ //Basic Enemy goes straight down
             y += 2;
             bottomy +=2;
         }
         
     }
-};
-struct ExplodingEnemy{ //Holds the x and y position of where an enemy was killed, so we 
-                        // know where to put the explosion
-    int x;
-    int y;
-    int opaqueness = 128;
-    
-    void updateframe(){
-        opaqueness -= 5;
+    //This will make isHit true 
+    void checkCollision(std::vector<Bullet>& firingbullets){
+        for(size_t i = 0; i < firingbullets.size(); i++){
+            
+            
+            if(firingbullets[i].x > x && firingbullets[i].x < bottomx && firingbullets[i].y < bottomy && firingbullets[i].y > y){
+                isHit = true;
+                enemyimage = explosion;
+                firingbullets.erase(firingbullets.begin() + i);
+                break;
+        }   
+        }
+        
     }
+    
+    
 };
+
+
+
+//-------------------
+// GLOBAL variables |
+//-------------------
+
+
+
+
+
+
+
 
 
 
@@ -88,15 +128,16 @@ int main(void)
     int userLives = 3;
     
     int backgroundY1 = 0;
-	int backgroundY2 = -900;
+	int backgroundY2 = -4900;
     
-    float speed = .22;
+    float speed = 3;
     
 
     InitWindow(screenWidth, screenHeight, "Galaca Game - Singplayer");
     Image icon = LoadImage("Graphics/user.png");
     SetWindowIcon(icon);
     UnloadImage(icon);
+    
     
     
     
@@ -109,11 +150,11 @@ int main(void)
     Texture2D background = LoadTexture("Graphics/background.png");
     Texture2D bulletimage = LoadTexture("Graphics/bullet.png");
     Texture2D enemyimage = LoadTexture("Graphics/enemy1.png");
-    Texture2D explosion = LoadTexture("Graphics/explosion.png");
     Texture2D ThreeLivesImage = LoadTexture("Graphics/HealthBar-3.png");
     Texture2D TwoLivesImage = LoadTexture("Graphics/HealthBar-2.png");
     Texture2D OneLivesImage = LoadTexture("Graphics/HealthBar-1.png");
     Texture2D ZeroLivesImage = LoadTexture("Graphics/HealthBar-0.png");
+    Texture2D explosion = LoadTexture("Graphics/explosion.png");
     
     //Logo Processing
     Texture2D logoimage = LoadTexture("Graphics/logo1.png");
@@ -121,18 +162,18 @@ int main(void)
     int logoy = 75;
     float logoOp = 255;
     
-    std::vector<Bullet> firingbullets; //This vector holds all active bullets on the screenHeight
     
-    // This holds enemy positions for the first wave
-    std::vector<BasicEnemy> level1;
-    std::vector<ExplodingEnemy> currentlyExploding;
+    
     
     
     
     
     //LEVEL 1 ENEMY LAYOUT
+    std::vector<BasicEnemy> level1; // This holds enemy positions for the first wave
+    
     int enemypos = 100;
-    for(int i = 0; i < 6; i++){
+    
+    for(int i = 0; i < 5; i++){
         BasicEnemy enemy;
         enemy.x = enemypos;
         enemy.y = -250;
@@ -141,9 +182,63 @@ int main(void)
         
         enemy.moveBacknForth = true;
         
+        
         level1.push_back(enemy);
         enemypos+=75;
     }
+    //Level 2 ENEMY LAYOUT
+    std::vector<BasicEnemy> level2; // This holds enemy positions for the first wave
+    enemypos = 50;
+    int enemyY = -450;
+    
+    for(int i = 0; i < 24; i++){
+        if (i == 8){
+            enemypos = 50;
+            enemyY = -500;
+        } else if (i == 16){
+            enemypos = 50;
+            enemyY = -550;
+        }
+        
+        BasicEnemy enemy;
+        enemy.x = enemypos;
+        enemy.y = enemyY;
+        enemy.bottomx = enemy.x+50;
+        enemy.bottomy = enemy.y+50;
+        
+        enemy.moveBacknForth = false;//Turn this true to make them shake
+        
+        level2.push_back(enemy);
+        enemypos+=65;
+    }
+    
+    //LEVEL 3 ENEMY LAYOUT
+        std::vector<BasicEnemy> level3; // This holds enemy positions for the first wave
+    enemypos = 50;
+    enemyY = -450;
+    
+    for(int i = 0; i < 18; i++){
+        if (i == 6){
+            enemypos = 25;
+            enemyY = -500;
+        } else if (i == 12){
+            enemypos = 75;
+            enemyY = -550;
+        }
+        
+        BasicEnemy enemy;
+        enemy.x = enemypos;
+        enemy.y = enemyY;
+        enemy.bottomx = enemy.x+50;
+        enemy.bottomy = enemy.y+50;
+        
+        enemy.moveBacknForth = true;//Turn this true to make them shake
+        
+        level3.push_back(enemy);
+        enemypos+=65;
+    }
+      
+    
     
 
     
@@ -169,64 +264,52 @@ int main(void)
         ClearBackground(BLACK);
         
         //MOVEMENT
-        bool moving = false; 
+        bool moving = false;
         
-        float dx, dy;//These will be used for the calculations for diagonal speed
-        
-        if (IsKeyDown(KEY_A)) dx -= 1;
-        if (IsKeyDown(KEY_D)) dx += 1;
-        if (IsKeyDown(KEY_W)) dy -= 1;
-        if (IsKeyDown(KEY_S)) dy += 1;
+        float dx = 0, dy = 0;
+        if (IsKeyDown(KEY_A) && userpositionx >= 0) dx -= 1;
+        if (IsKeyDown(KEY_D) && userpositionx <= 530) dx += 1;
+        if (IsKeyDown(KEY_W) && userpositiony >= 200) dy -= 1;
+        if (IsKeyDown(KEY_S) && userpositiony <= 865) dy += 1;
 
-        if (dx != 0 || dy != 0) {
-            userpositionx += dx * speed;
-            userpositiony += dy * speed;
+        if (dx != 0 && dy != 0) {
+            dx *= 0.70710678f;
+            dy *= 0.70710678f;
+        }
 
-            // Clamp to screen bounds
-            if (userpositionx < 0)   userpositionx = 0;
-            if (userpositionx > 530) userpositionx = 530;
-            if (userpositiony < 200) userpositiony = 200;
-            if (userpositiony > 865) userpositiony = 865;
-
-            if (dx < 0)      currentusersprite = userspriteleft;
-            else if (dx > 0) currentusersprite = userspriteright;
-            else             currentusersprite = usersprite;
-                 } else {
-                    currentusersprite = usersprite;
-                    }
-        
+        userpositionx += dx * speed;
+        userpositiony += dy * speed;
         
         if (IsKeyDown(KEY_A)) {// This code runs only once each frame, when a button is pressed 
-            
-            if (userpositionx != 0){// Sets the border ie. cannot go too far left
+            if (userpositionx >= 0){// Sets the border ie. cannot go too far left
             userpositionx-= speed;
             currentusersprite = userspriteleft;
             moving = true;
             }
             
-        }  
+        } 
         if (IsKeyDown(KEY_D)){// Sets the border ie. cannot go too far right
-            if (userpositionx != 530){
+            if (userpositionx <= 530){
             userpositionx+= speed;
             currentusersprite = userspriteright;
             moving = true;
             }
         }
         if (IsKeyDown(KEY_W)) {
-            if (userpositiony != 200){
+            if (userpositiony >= 200){
                 userpositiony -= speed;
                 currentusersprite = usersprite;
                 moving = true;
             }
         }
         if (IsKeyDown(KEY_S)){
-            if (userpositiony != 865){
+            if (userpositiony <= 865){
                 userpositiony += speed;
                 currentusersprite = usersprite;
                 moving = true;
             }
         } 
-        if(!moving){
+        if (!moving){
             currentusersprite = usersprite;
         }
         
@@ -254,14 +337,14 @@ int main(void)
         
         // TODO: Update your variables here
         //----------------------------------------------------------------------------------
-		backgroundY2 += 10; // Moves the background down
-        backgroundY1 += 10;  // Move first background down
+		backgroundY2 += 17; // Moves the background down
+        backgroundY1 += 17;  // Move first background down
         
-        if (backgroundY1 >= 900) {
-			backgroundY1 = backgroundY2 - 900;
+        if (backgroundY1 >= 4900) {
+			backgroundY1 = backgroundY2 - 4900;
 		}
-		if (backgroundY2 >= 900) {
-			backgroundY2 = backgroundY1 - 900;
+		if (backgroundY2 >= 4900) {
+			backgroundY2 = backgroundY1 - 4900;
 		}
         
         
@@ -275,48 +358,90 @@ int main(void)
             
             DrawTexture(currentusersprite, userpositionx, userpositiony, WHITE);//Draws users position
             
-
-            
+            //The bool will tell which level to start drawing. 
+            bool RunLevel2 = false;
+            bool RunLevel3 = false;
             //loop that draws all enemies in level1
-            for(int i = (int)level1.size() - 1; i >= 0; i--){
+            int i = 0;
+            while(i < level1.size() && level1.size() != 0){
                 
-                bool hit = false;
-                for(int j = (int)firingbullets.size() - 1; j >= 0; j--){
-                    
-                    if(firingbullets[j].x > level1[i].x && firingbullets[j].x < level1[i].bottomx && firingbullets[j].y < level1[i].bottomy && firingbullets[j].y > level1[i].y){
-                        
-                        //This is where the collision happens
-                        ExplodingEnemy exploding;
-                        exploding.x = level1[i].x;
-                        exploding.y = level1[i].y;
-                        
-                        currentlyExploding.push_back(exploding);
-                        
-                        hit = true;
-                        firingbullets.erase(firingbullets.begin() + j);
-                        break;
-                        
-                    }
-                    
-                }
-              
-                if(!hit) {  // Only draw and update if NOT hit
-                    DrawTexture(level1[i].enemyimage, level1[i].x, level1[i].y, WHITE);
                     level1[i].updateframe();
+                    
+                    DrawTexture(level1[i].enemyimage, level1[i].x, level1[i].y,(Color){255, 255, 255, (unsigned char)level1[i].opaqueness});
+                    
                     
                     if(level1[i].bottomy > 895){
                         userLives--;
                         level1.erase(level1.begin() + i);
+                    } else if(level1[i].dead){
+                        level1.erase(level1.begin() + i);
                     }
+
+                    //If you make a user collision with the enemy do it here
                     
-                    
-                } else {
-                    // Draw explosion here if you want
-                    level1.erase(level1.begin() + i);
+                i++;
+                if(i > level1.size()){
+                    i = 0;
                 }
-                    
+                continue;
+            }
+            
+            //Loop for level 2
+            if(level1.size() == 0){
+                int i = 0;
+                //RunLevel2 = true;
+                RunLevel2 = true;
                 
             }
+            
+            while(RunLevel2 && i < level2.size() && level2.size() != 0){
+                
+                level2[i].updateframe();
+                
+                DrawTexture(level2[i].enemyimage,level2[i].x,level2[i].y,(Color) {255,255,255, (unsigned char)level2[i].opaqueness});
+                
+                if(level2[i].bottomy > 895){
+                    userLives--;
+                    level2.erase(level2.begin() + i);
+                } else if(level2[i].dead){
+                        level2.erase(level2.begin() + i);
+                    }
+                
+                i++;
+                if(i > level2.size()){
+                    i = 0;
+                }
+                continue;
+                
+            }
+            
+            //Loop for level 3
+            if(level2.size() == 0){
+                int i = 0;
+                RunLevel3 = true;
+            }
+            
+            while(RunLevel3 && i < level3.size() && level3.size() != 0){
+                
+                level3[i].updateframe();
+                
+                DrawTexture(level3[i].enemyimage,level3[i].x,level3[i].y,(Color) {255,255,255, (unsigned char)level3[i].opaqueness});
+                
+                if(level3[i].bottomy > 895){
+                    userLives--;
+                    level3.erase(level3.begin() + i);
+                } else if(level3[i].dead){
+                        level3.erase(level3.begin() + i);
+                    }
+                
+                i++;
+                if(i > level3.size()){
+                    i = 0;
+                }
+                continue;
+                
+            }
+            
             
             //loop that draws all bullets
             for(size_t i = 0; i < firingbullets.size(); i++){
@@ -329,15 +454,7 @@ int main(void)
                 DrawTexture(bulletimage, firingbullets[i].x, firingbullets[i].y, WHITE);   
             }
             
-            for(size_t i = 0; i < currentlyExploding.size(); i++){
-                if(currentlyExploding[i].opaqueness <= 10){
-                    currentlyExploding.erase(currentlyExploding.begin() + (int)i);
-                }                    
-                DrawTexture(explosion,currentlyExploding[i].x, currentlyExploding[i].y, (Color){255, 255, 255, (unsigned char)currentlyExploding[i].opaqueness});
-                currentlyExploding[i].updateframe();
-                
-                
-            }
+
             
             
             
